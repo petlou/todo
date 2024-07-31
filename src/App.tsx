ca0276f333c9179b271bdf8e6707335969ca48e2
Peter Lourenço <peter.lourenco@brisabr.com.br>
2024-07-31 19:45:12 -0300

    feat: Configuração do projeto de exemplo

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/_globals.scss'
+
+function App() {
+	return (
+		<section>
+			<h1>Hello Todo App!</h1>
+			<p>Teste de estilo de fonte.</p>
+		</section>
+	)
+}
+
+export default App
